1c1abe535f7051d058de92f53370a789c13533ed
Quan Zhang <112660122+dmdqy@users.noreply.github.com>
2024-03-29 18:12:23 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name='pathermo',
+    version='0.0.1',
+    author='Quan Zhang',
+    description='Blank',
+    long_description='This is a longer description for the project',
+    url='https://github.com/dmdqy/pathermo',
+    keywords='example',
+    python_requires='>=3',
+    install_requires=[],    
+    package_data={
+        'sample': [],
+    },    
+)
